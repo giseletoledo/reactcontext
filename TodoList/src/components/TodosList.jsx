20c002540b7e0b59d1ace824f2e565b802c107b9
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-02-05 19:18:25 +0000

    Ajustando componentes filhos com context

--- TodoList/src/components/TodosList.jsx
+++ TodoList/src/components/TodosList.jsx
@@ -1,24 +1,20 @@
 import Todo from './Todo.jsx';
-import {useState} from 'react';
+import { useContext } from "react";
+import { TodosContext } from "../TodosContext.js";
 
-const initialTodos = [
-  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
-  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
-  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
-];
 
 function TodosList() {
 
-  const [todos, setTodos] = useState(initialTodos);
+  const store = useContext(TodosContext);
 
   function deleteHandler(id) {
     if (confirm('Are you sure you want to delete the todo?')) {
-      setTodos(todos.filter(todo => todo.id !== id));    
+      store.setTodos(todos.filter(todo => todo.id !== id));    
     }
   }
 
   function toggleIsDoneHandler(id) {
-    setTodos(todos.map(todo => {
+    store.setTodos(store.todos.map(todo => {
       if (todo.id === id) {
         todo.isDone = !todo.isDone;
         return todo;
@@ -31,7 +27,7 @@ function TodosList() {
   return (
     <>
         <div className="todos">
-          {todos.map(todo => 
+          {store.todos.map(todo => 
             <Todo
               deleteTodo={(id) => deleteHandler(id)}
               toggleIsDone={(id) => toggleIsDoneHandler(id)}
