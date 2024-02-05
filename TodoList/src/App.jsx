23444f852c4fb21c294f0a317649e0ac6aaad7c3
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-02-05 19:02:28 +0000

    Usando context lift up state

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,17 +1,28 @@
 import Header from './components/Header.jsx';
 import Home from './views/Home.jsx';
+import { TodosContext } from "./TodosContext.js";
+import { useState } from "react";
+
 import './App.scss';
 
+const initialTodos = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
 function App() {
+
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <>
       <main>
-        
+        <TodosContext.Provider value={{todos,setTodos}}>
         <Header appName="To-Do List with React" />
 
         <Home />
-
+        </TodosContext.Provider>
       </main>
     </>
   )
